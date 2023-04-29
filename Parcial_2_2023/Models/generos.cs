5b3b1143d088ea52ab660b6020608186d33722d1
Jefferson545 <jefferson.avelar@catolica.edu.sv>
2023-04-29 08:30:10 -0600

    clases y context

---
+++ Parcial_2_2023/Models/generos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace P2_2020ZA601_2020AA603_2020SC603.Models
+{
+    public class generos
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id_genero { get; set; }
+        [Display(Name = "Genero")]
+        public string? genero { get; set; }
+    }
+}
