fa5689cd2594f70fb0f61d12f3317bb45eb76818
Miguel <miguel.zelaya@catolica.edu.sv>
2023-04-29 11:22:08 -0600

    Terminado

--- Parcial_2_2023/Controllers/CovidController.cs
+++ Parcial_2_2023/Controllers/CovidController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020ZA601_2020AA603_2020SC603.Models;
 using Parcial_2_2023.Models;
+
 
 namespace Parcial_2_2023.Controllers
 {
@@ -13,12 +15,39 @@ namespace Parcial_2_2023.Controllers
         }
         public IActionResult Index()
         {
-            var listadodepartamentos=(from m in _covidDbContext.casos_reportados join n in _covidDbContext.departamentos
-                                      on m.id_departamento equals n.id_departamento
+            var listadodepartamentos=(from m in _covidDbContext.departamentos
                                       select m).ToList();
             ViewData["listadodepartamentos"] = new SelectList(listadodepartamentos, "id_departamento", "nombre_departamento");
+
+            var listadogenero = (from m in _covidDbContext.generos
+                                 select m).ToList();
+            ViewData["listadogenero"] = new SelectList(listadogenero, "id_genero", "genero");
+
+            var listadocovids = (from c in _covidDbContext.casos_reportados
+                                 join d in _covidDbContext.departamentos on c.id_departamento equals d.id_departamento
+                                 join g in _covidDbContext.generos on c.id_genero equals g.id_genero
+                                 select new
+                                 {
+                                     departamentos = d.nombre_departamento,
+                                     genero = g.genero,
+                                     confirmados = c.confirmados,
+                                     recuperados = c.recuperados,
+                                     fallecidos = c.fallecidos,
+
+                                 }).ToList();
+            ViewData["listadocovid"] = listadocovids;
+
             return View();
+            
+            
         }
+        public IActionResult CrearDatos(casos_reportados nuevoDato)
+        {
+            _covidDbContext.Add(nuevoDato);
+            _covidDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
--- Parcial_2_2023/Program.cs
+++ Parcial_2_2023/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Covid}/{action=Index}/{id?}");
 
 app.Run();
