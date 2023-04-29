2e973a8593f55e1768a5af4fbbcb137b36e7cba8
csilva270801 <cescloborojo20@hotmail.com>
2023-04-29 08:32:57 -0600

    casos_reportados

--- Parcial_2_2023/Models/casos_reportados.cs
+++ Parcial_2_2023/Models/casos_reportados.cs
@@ -1,6 +1,21 @@
-﻿namespace P2_2020ZA601_2020AA603_2020SC603.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_2020ZA601_2020AA603_2020SC603.Models
 {
     public class casos_reportados
     {
+        [Key]
+
+        [Display(Name = "ID Departamento")]
+        public int id_departamento { get; set; }
+        [Display(Name = "ID Genero")]
+        public int id_genero { get; set; }
+        [Display(Name = "Confirmados")]
+        public int confirmados { get; set; }
+        [Display(Name = "Recuperados")]
+        public int recuperados { get; set; }
+        [Display(Name = "Fallecidos")]
+        public int fallecidos { get; set; }
+
     }
 }
