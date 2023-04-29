b927334590f134d76394959885ccbcc7126db993
csilva270801 <cescloborojo20@hotmail.com>
2023-04-29 09:57:59 -0600

    ??

--- Parcial_2_2023/Models/casos_reportados.cs
+++ Parcial_2_2023/Models/casos_reportados.cs
@@ -8,13 +8,13 @@ namespace P2_2020ZA601_2020AA603_2020SC603.Models
 
         public int id_departamento { get; set; }
 
-        public int id_genero { get; set; }
+        public int? id_genero { get; set; }
 
-        public int confirmados { get; set; }
+        public int? confirmados { get; set; }
 
-        public int recuperados { get; set; }
+        public int? recuperados { get; set; }
  
-        public int fallecidos { get; set; }
+        public int? fallecidos { get; set; }
 
     }
 }
